79b462b707dcdf6acc0e6bbf6d6f8bafdd787b1f
wmedrano <wmedrano@google.com>
2026-03-03 09:27:21 -0800

    Expose undefined behavior when size is 0

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ fn main() {
     let mut builder = cc::Build::new();
     #[cfg(feature = "stack-clash-protection")]
     builder.flag_if_supported("-fstack-clash-protection");
+    builder.flag_if_supported("-fsanitize=undefined");
+    println!("cargo:rustc-link-arg=-fsanitize=undefined");
     (if option_env!("CC") == Some("clang") {
         builder.flag("-flto")
     } else {
